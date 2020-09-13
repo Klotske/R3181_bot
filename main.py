f8c8fdc3211a444742ffcb16fa262bff3be1eb1e
Klotske <ilya@mokhov.vg>
2020-09-14 02:01:35 +0300

    Добавлен базовый webhook обработчик Telegram

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import telegram, settings, vk_api, tg_api
 
 app = Flask(__name__)
 
+bot = telegram.Bot(token=settings.tg_token)
+
 @app.route('/')
 def main():
     return "Hello World"
@@ -27,4 +29,17 @@ def vk_hook():
                 vk_api.sendMessage(user_id, "Привет!")
         except (KeyError, TypeError) as e:
             print("(VK) Error: ", e, sys.exc_info()[0], sys.exc_info()[1])
-    
+
+@app.route('/tg/', methods=["POST"])
+def tg_hook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    try:
+        chat_id = update.message.chat.id
+        user_id = update.message.from_user.id
+        text = update.message.text.encode("utf-8").decode()
+        msg = text.lower()
+        print("(TG) ID пользователя", user_id, "Сообщение: ", text)
+        if msg in ['/start', 'привет']:
+            bot.sendMessage(chat_id, "Привет")
+    except (KeyError, TypeError) as e:
+        print("(TG) Error: ", e, sys.exc_info()[0], sys.exc_info()[1])
