3cb88fa89a1eab891dea3b4eb912746d0a39c558
Klotske <ilya@mokhov.vg>
2020-09-14 01:34:25 +0300

    Добавлена разметка клавиатур для Telegram

--- tg_api.py
+++ tg_api.py
@@ -1,3 +1,21 @@
 from telegram import ReplyKeyboardMarkup
 
 # Место для разметки пользовательских меню
+
+menuMain = ReplyKeyboardMarkup(
+    [
+        ["Актуальное"],
+        ["Мои задания", "Настройки"],
+        ["Полезные материалы"]
+    ],
+    one_time_keyboard=False,
+    resize_keyboard=True
+)
+
+back = ReplyKeyboardMarkup(
+    [
+        ["Назад"]
+    ],
+    one_time_keyboard=False,
+    resize_keyboard=True
+)
